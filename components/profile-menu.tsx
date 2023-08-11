ede273ae3123a6a3faf2189836a0f57456e03ea4
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-08-11 15:56:15 +0530

    added icon on profile menu

--- components/profile-menu.tsx
+++ components/profile-menu.tsx
@@ -10,6 +10,7 @@ import {
 import { signOut } from "next-auth/react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import Link from "next/link";
+import { LogOut, Milestone, Settings } from "lucide-react";
 
 const ProfileDropdownMenu = ({ session }: { session: SessionInterface }) => {
   return (
@@ -23,19 +24,23 @@ const ProfileDropdownMenu = ({ session }: { session: SessionInterface }) => {
       <DropdownMenuContent className="w-full">
         <DropdownMenuItem>{session?.user?.name}</DropdownMenuItem>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>
+        <DropdownMenuItem className="group">
+          <Milestone className="w-5 h-5 mr-3 text-zinc-200 dark:text-zinc-600 group-hover:text-primary" />
           <Link href={`/profile/${session?.user?.id}`} className="text-sm">
             Profile
           </Link>
         </DropdownMenuItem>
-        <DropdownMenuItem>
+        <DropdownMenuItem className="flex group">
+          <Settings className="w-5 h-5 mr-3 text-zinc-200 dark:text-zinc-600 group-hover:text-primary" />
           <Link href={`/settings/${session?.user?.id}`} className="text-sm">
             Settings
           </Link>
         </DropdownMenuItem>
 
         <DropdownMenuSeparator />
-        <DropdownMenuItem onClick={() => signOut()}>Sign out</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => signOut()} className="group">
+        <LogOut className="w-5 h-5 mr-3 text-zinc-200 dark:text-zinc-600 group-hover:text-primary" /> Sign out
+          </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
