38bc9b5f0886552cc3acae4e83e8feb92581d2ac
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-08-15 02:11:18 +0530

    ...possible fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import Footer from "@/components/footer";
-import Navbar from "@/components/navbar";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import { siteConfig } from "@/config/site";
