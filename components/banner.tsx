20b66c3c57fd4f2538848d74662ea2e1d629c0ab
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-08-15 00:30:19 +0530

    fixed banner styling issue and update hashnodeLink

--- components/banner.tsx
+++ components/banner.tsx
@@ -3,39 +3,48 @@
 const Banner = () => {
   return (
     <>
-      <div className="inline-flex items-center px-3 py-1 text-sm font-medium rounded-lg bg-muted">
+      <div className="flex flex-col items-center px-3 py-1 text-sm font-medium rounded-lg md:flex-row bg-muted">
         This is my submission for{" "}
-        <a
-          href="https://grafbase.com/"
-          target="_blank"
-          className="hover:text-blue-500"
-        >
-          &nbsp; Grafbase&nbsp;
-        </a>{" "}
-        X{" "}
-        <a
-          href="https://hashnode.com/"
-          target="_blank"
-          className="hover:text-blue-500"
-        >
-          &nbsp;Hashnode Hackathon
-        </a>
+        <div>
+          <a
+            href="https://grafbase.com/"
+            target="_blank"
+            className="hover:text-blue-500 "
+          >
+            &nbsp; Grafbase&nbsp;
+          </a>{" "}
+          X{" "}
+          <a
+            href="https://hashnode.com/"
+            target="_blank"
+            className="hover:text-blue-500"
+          >
+            &nbsp;Hashnode
+          </a>
+          <a
+            href="https://hashnode.com/hackathons/grafbase"
+            target="_blank"
+            className="hover:text-blue-500"
+          >
+            &nbsp; Hackathon
+          </a>
+        </div>
       </div>
       <div className="flex">
         <div className="flex items-center justify-center px-3 py-1 mx-5 mt-5 text-sm font-medium rounded-lg w-fit bg-muted">
           <a
             href="https://github.com/trace2798/grafbase_fullstack"
             target="_blank"
-            className="hover:text-blue-500"
+            className="text-center hover:text-blue-500"
           >
             &nbsp;Github Repo&nbsp;
           </a>
         </div>
         <div className="flex flex-col items-center px-3 py-1 mx-5 mt-5 text-sm font-medium rounded-lg bg-muted">
           <a
-            href="hashnode.com"
+            href="https://shreyas-chaliha.hashnode.dev/post-your-thought-with-graphy"
             target="_blank"
-            className="hover:text-blue-500"
+            className="text-center hover:text-blue-500"
           >
             Hashnode Article
           </a>
